6255db9abc30570cbcabc8b6e30a55cff6911030
math-vrn <vnikitin90@gmail.com>
2019-01-07 18:51:17 -0600

    test figures

--- test_s.py
+++ test_s.py
@@ -48,9 +48,6 @@ if __name__ == "__main__":
     rho = 0.5
     gamma = 0.25
     eta = 0.25
-    piter = 1
-    titer = 1
-    NITER = 2
     voxelsize = 1e-6
     energy = 5
 
@@ -67,9 +64,13 @@ if __name__ == "__main__":
     theta = np.linspace(0, 2*np.pi, 720).astype('float32')
 
     if(sys.argv[1] == "True"):
+
         print('compute regular')
         maxint = 100
         rho = 1e-10
+        piter = 250
+        titer = 250
+        NITER = 1
         # Create probe.
         prb = objects.Probe(gaussian(15, rin=0.8, rout=1.0), maxint=maxint)
         # Raster scan parameters for each rotation angle.
@@ -98,14 +99,38 @@ if __name__ == "__main__":
         x = slv.admm(data, h, psi, lamd, x, rho,
                      gamma, eta, piter, titer, NITER)
 
-        # Save result
-        dxchange.write_tiff(x.beta,  '../rec_ptycho/beta_s/beta_joint_60over')
-        dxchange.write_tiff(
-            x.delta,  '../rec_ptycho/delta_s/delta_joint_60over')
 
-    maxinta = [100, 10, 1, 0.1]
-    idc = np.int(sys.argv[2])
-    for k in range(idc, idc+1):
+        # Save result
+        dxchange.write_tiff(x.beta,  '../rec_ptycho/beta_s/beta_st_over12')
+        dxchange.write_tiff(
+            x.delta,  '../rec_ptycho/delta_s/delta_st_over12')
+
+
+        rho = 0.5
+        piter = 1
+        titer = 1
+        NITER = 250
+        # rec
+        h = np.ones(tomoshape, dtype='complex64', order='C')
+        psi = np.ones(tomoshape, dtype='complex64', order='C')
+        lamd = np.zeros(psi.shape, dtype='complex64', order='C')
+        mu = np.zeros([3, *obj.shape], dtype='complex64', order='C')
+        x = objects.Object(np.zeros(obj.shape, dtype='float32', order='C'), np.zeros(
+            obj.shape, dtype='float32', order='C'), voxelsize)
+
+        # ADMM
+        x = slv.admm(data, h, psi, lamd, x, rho,
+                     gamma, eta, piter, titer, NITER)
+
+        # Save result
+        dxchange.write_tiff(x.beta,  '../rec_ptycho/beta_s/beta_joint_over12')
+        dxchange.write_tiff(
+            x.delta,  '../rec_ptycho/delta_s/delta_joint_over12')
+
+
+
+    maxinta = [100,10,1,0.1]
+    for k in len(maxinta):
         maxint = maxinta[k]
         # Create probe.
         prb = objects.Probe(gaussian(15, rin=0.8, rout=1.0), maxint=maxint)
@@ -125,6 +150,11 @@ if __name__ == "__main__":
         data = np.random.poisson(
             data*det.x*det.y).astype('float32')/(det.x*det.y)
 
+
+        rho = 1e-10
+        piter = 250
+        titer = 250
+        NITER = 1
         # rec
         h = np.ones(tomoshape, dtype='complex64', order='C')
         psi = np.ones(tomoshape, dtype='complex64', order='C')
@@ -138,6 +168,28 @@ if __name__ == "__main__":
                      gamma, eta, piter, titer, NITER)
 
         dxchange.write_tiff(
-            x.beta,   '../rec_ptycho/beta/beta_joint_60over_' + str(maxint)+'_maxint_noise')
+            x.beta,   '../rec_ptycho/beta/beta_st_over12_' + str(maxint)+'_maxint_noise')
         dxchange.write_tiff(
-            x.delta,  '../rec_ptycho/delta/delta_joint_60over_'+str(maxint)+'_maxint_noise')
+            x.delta,  '../rec_ptycho/delta/delta_st_over12_'+str(maxint)+'_maxint_noise')
+
+
+        rho = 0.5
+        piter = 250
+        titer = 250
+        NITER = 250
+        # rec
+        h = np.ones(tomoshape, dtype='complex64', order='C')
+        psi = np.ones(tomoshape, dtype='complex64', order='C')
+        lamd = np.zeros(psi.shape, dtype='complex64', order='C')
+        mu = np.zeros([3, *obj.shape], dtype='complex64', order='C')
+        x = objects.Object(np.zeros(obj.shape, dtype='float32', order='C'), np.zeros(
+            obj.shape, dtype='float32', order='C'), voxelsize)
+
+        # ADMM
+        x = slv.admm(data, h, psi, lamd, x, rho,
+                     gamma, eta, piter, titer, NITER)
+
+        dxchange.write_tiff(
+            x.beta,   '../rec_ptycho/beta/beta_joint_over12_' + str(maxint)+'_maxint_noise')
+        dxchange.write_tiff(
+            x.delta,  '../rec_ptycho/delta/delta_joint_over12_'+str(maxint)+'_maxint_noise')
