639990982bc01c4f83a2b16bae2d091d0a508114
math-vrn <vnikitin90@gmail.com>
2019-01-09 19:26:37 -0600

    clean code

--- solver_gpu.py
+++ solver_gpu.py
@@ -3,8 +3,6 @@
 
 """Module for 3D ptychography."""
 
-import dxchange
-import tomopy
 import radonusfft
 import ptychofft
 import numpy as np
@@ -18,9 +16,8 @@ SPEED_OF_LIGHT = 299792458e+2  # [cm/s]
 
 
 class Solver(object):
-    def __init__(self, prb, scan, scanax, scanay, theta, det, voxelsize, energy, tomoshape):
+    def __init__(self, prb, scanax, scanay, theta, det, voxelsize, energy, tomoshape):
         self.prb = prb
-        self.scan = scan
         self.scanax = scanax
         self.scanay = scanay
         self.theta = theta
@@ -40,21 +37,19 @@ class Solver(object):
         self.theta_gpu = tomoshape[0]//10
         self.cl_ptycho = ptychofft.ptychofft(self.theta_gpu, tomoshape[1], tomoshape[2],
                                              scanax.shape[1], scanay.shape[1], det.x, det.y, prb.size)
-        print(self.objshape)
-        print(self.ptychoshape)
 
     def wavenumber(self):
         return 2 * np.pi / (2 * np.pi * PLANCK_CONSTANT * SPEED_OF_LIGHT / self.energy)
 
-    # exp representation of R
+    # Exp representation of projections, exp(i\nu\psi)
     def exptomo(self, psi):
         return np.exp(1j * psi * self.voxelsize * self.wavenumber())
 
-    # log represnetation of R
+    # Log representation of projections, -i/\nu log(psi)
     def logtomo(self, psi):
         return -1j / self.wavenumber() * np.log(psi) / self.voxelsize
 
-    # Radon transform (R )
+    # Radon transform (R)
     def fwd_tomo(self, psi):
         res_gpu = np.zeros(self.tomoshape, dtype='complex64', order='C')
         self.cl_tomo.fwd(res_gpu, psi)
@@ -75,7 +70,6 @@ class Solver(object):
             self.cl_ptycho.setobj(self.scanax[ast:aend], self.scanay[ast:aend],
                                   self.prb.complex)
             self.cl_ptycho.fwd(res_gpu[ast:aend], psi[ast:aend])
-
         return res_gpu
 
     # adjoint ptychography transfrorm (Q*F*)
@@ -113,29 +107,25 @@ class Solver(object):
     # Gradient descent tomography
     def grad_tomo(self, data, niter, init, rho, eta):
         # normalization coefficient
-        r = 1/np.sqrt(data.shape[0]*data.shape[1]/2)
-        res = init.complexform
-        for i in range(niter):
-            tmp0 = self.fwd_tomo(res)
-            tmp = self.adj_tomo(2*(tmp0-data))
-            res = res - eta*r*r*tmp
-        return objects.Object(res.imag, res.real, self.voxelsize)
-
-    # Gradient descent ptychography
-
-    def grad_ptycho(self, data, init, niter, rho, gamma, hobj, lamd):
-        psi = init.copy()
-
-        # psi2 = init.copy()
-        # for i in range(niter):
-        #     tmp = self.fwd_ptycho(psi2)
-        #     tmp = self.update_amp(tmp, data)
-        #     upd1 = self.adj_ptycho(tmp)
-        #     upd2 = self.adjfwd_prb(psi2)
-        #     psi2 = (1 - rho*gamma) * psi2 + rho*gamma * \
-        #         (hobj - lamd/rho) + (gamma / 2) * (upd1-upd2) / self.maxint
+        r = 1/np.sqrt(data.shape[0]*data.shape[2]/2)
 
         # whole scheme on gpu
+        res_gpu = init.complexform
+        self.cl_tomo.grad_tomo(res_gpu, data, r*r*eta, niter)
+
+        # # Alternative computation
+        # res = init.complexform
+        # for i in range(niter):
+        #     tmp0 = self.fwd_tomo(res)
+        #     tmp = self.adj_tomo(tmp0-data)
+        #     res = res - 2*eta*r*r*tmp
+        # print(np.linalg.norm(res-res_gpu)/np.linalg.norm(res))
+        return objects.Object(res_gpu.imag, res_gpu.real, self.voxelsize)
+
+    # Gradient descent ptychography
+    def grad_ptycho(self, data, init, niter, rho, gamma, hobj, lamd):
+        # whole scheme on gpu
+        psi = init.copy()
         for k in range(0, self.tomoshape[0]//self.theta_gpu):
             # process self.theta_gpu angles on 1gpu simultaneously
             ast, aend = k*self.theta_gpu, (k+1)*self.theta_gpu
@@ -144,38 +134,42 @@ class Solver(object):
             self.cl_ptycho.grad_ptycho(
                 psi[ast:aend], data[ast:aend], hobj[ast:aend], lamd[ast:aend], rho, gamma, self.maxint, niter)
 
-        # print(np.linalg.norm(psi2-psi))
-        # print(np.linalg.norm(psi))
+        ## Alternative computation
+        # psi2 = init.copy()
+        # for i in range(niter):
+        #     tmp = self.fwd_ptycho(psi2)
+        #     tmp = self.update_amp(tmp, data)
+        #     upd1 = self.adj_ptycho(tmp)
+        #     upd2 = self.adjfwd_prb(psi2)
+        #     psi2 = (1 - rho*gamma) * psi2 + rho*gamma * \
+        #         (hobj - lamd/rho) + (gamma / 2) * (upd1-upd2) / self.maxint
+        ## Compare results
+        # print(np.linalg.norm(psi2-psi)/np.linalg.norm(psi))
         return psi
 
-    #@profile
     # ADMM for ptycho-tomography problem
     def admm(self, data, h, psi, lamd, x, rho, gamma, eta, piter, titer, NITER):
         for m in range(NITER):
+            # keep previous iteration
             psi0, x0 = psi, x
             # psi update
             psi = self.grad_ptycho(data, psi, piter, rho, gamma, h, lamd)
-            #dxchange.write_tiff(psi.real,"../rec_ptycho/psir15")
-            #dxchange.write_tiff(psi.imag,"../rec_ptycho/psii15")
             # x update
-            x = self.grad_tomo(self.logtomo(psi+lamd/rho), titer, x, rho, eta)
+            tmp = self.logtomo(psi+lamd/rho)
+            x = self.grad_tomo(tmp, titer, x, rho, eta)
             # h update
             h = self.exptomo(self.fwd_tomo(x.complexform))
             # lambda update
             lamd = lamd + rho * (psi - h)
 
-            # # check convergence of the Lagrangian
-            # if (np.mod(m, 1) == 0):
-            #     terms = np.zeros(4, dtype='float32')  # ignore imag part
-            #     terms[0] = 0.5 * np.linalg.norm(
-            #         np.abs(self.fwd_ptycho(psi))-np.sqrt(data))**2
-            #     terms[1] = np.sum(np.conj(lamd)*(psi-h))
-            #     terms[2] = 0.5*rho*np.linalg.norm(psi-h)**2
-            #     terms[3] = np.sum(terms[0:3])
-
-            #     print("%d) Lagrangian terms:  %.2e %.2e %.2e %.2e" %
-            #           (m, terms[0], terms[1], terms[2], terms[3]))
-            # check convergence of psi and x
-            #print("%d) Conv psi, x:  %.2e %.2e" % (m, np.linalg.norm(psi-psi0),np.linalg.norm(x.complexform-x0.complexform)))
+            # check convergence of the Lagrangian
+            if (np.mod(m, 16) == 0):
+                terms = np.zeros(3, dtype='float32')  # ignore imag part
+                terms[0] = 0.5 * np.linalg.norm(
+                    np.abs(self.fwd_ptycho(psi))-np.sqrt(data))**2
+                terms[1] = np.sum(np.conj(lamd)*(psi-h))
+                terms[2] = 0.5*rho*np.linalg.norm(psi-h)**2
+                print("%d) Lagrangian terms:  %.2e %.2e %.2e sum = %.2e" %
+                      (m, terms[0], terms[1], terms[2], np.sum(terms)))
 
         return x
--- test.py
+++ test.py
@@ -3,47 +3,13 @@ import solver_gpu
 import dxchange
 import tomopy
 import numpy as np
+import signal
+import sys
 
 
-def gaussian(size, rin=0.8, rout=1):
-    r, c = np.mgrid[:size, :size] + 0.5
-    rs = np.sqrt((r - size/2)**2 + (c - size/2)**2)
-    rmax = np.sqrt(2) * 0.5 * rout * rs.max() + 1.0
-    rmin = np.sqrt(2) * 0.5 * rin * rs.max()
-    img = np.zeros((size, size), dtype='float32')
-    img[rs < rmin] = 1.0
-    img[rs > rmax] = 0.0
-    zone = np.logical_and(rs > rmin, rs < rmax)
-    img[zone] = np.divide(rmax - rs[zone], rmax - rmin)
-    return img
-
-
-def scanner3(theta, shape, sx, sy, margin=[0, 0], offset=[0, 0], spiral=0):
-    a = spiral
-    scan = []
-    lenmaxx = 0
-    lenmaxy = 0
-
-    for m in range(len(theta)):
-        s = objects.Scanner(shape, sx, sy, margin, offset=[
-                            offset[0], np.mod(offset[1]+a, sy)])
-        scan.append(s)
-        a += spiral
-        lenmaxx = max(lenmaxx, len(s.x))
-        lenmaxy = max(lenmaxy, len(s.y))
-
-    scanax = -1+np.zeros([len(theta), lenmaxx], dtype='int32')
-    scanay = -1+np.zeros([len(theta), lenmaxy], dtype='int32')
-
-    for m in range(len(theta)):
-        scanax[m, :len(scan[m].x)] = scan[m].x
-        scanay[m, :len(scan[m].y)] = scan[m].y
-
-    return scan, scanax, scanay
-
-
+    
 if __name__ == "__main__":
-
+    
     # Parameters.
     rho = 0.5
     gamma = 0.25
@@ -55,7 +21,7 @@ if __name__ == "__main__":
     voxelsize = 1e-6
     energy = 5
 
-    # Load a 3D object.
+    # Load a 3D object
     beta = dxchange.read_tiff(
         'data/test-beta-128.tiff').astype('float32')[::2, ::2, ::2]
     delta = dxchange.read_tiff(
@@ -63,37 +29,43 @@ if __name__ == "__main__":
 
     # Create object.
     obj = objects.Object(beta, delta, voxelsize)
-    # Create probe.
-    prb = objects.Probe(gaussian(15, rin=0.8, rout=1.0), maxint=maxint)  
-    # Detector parameters.
+    # Create probe
+    prb = objects.Probe(objects.gaussian(15, rin=0.8, rout=1.0), maxint=maxint)
+    # Detector parameters
     det = objects.Detector(63, 63)
-    # Define rotation angles.
+    # Define rotation angles
     theta = np.linspace(0, 2*np.pi, 720).astype('float32')
-    # Raster scan parameters for each rotation angle.
-    scan, scanax, scanay = scanner3(theta, beta.shape, 6, 200, margin=[
-                                    prb.size, prb.size], offset=[0, 0], spiral=1)
+    # Scanner positions
+    scanax, scanay = objects.scanner3(theta, beta.shape, 12, 12, margin=[
+        prb.size, prb.size], offset=[0, 0], spiral=1)
+    # tomography data shape
     tomoshape = [len(theta), obj.shape[0], obj.shape[2]]
-
-    # class solver
-    slv = solver_gpu.Solver(prb, scan, scanax, scanay,
+    
+    # Class solver    
+    slv = solver_gpu.Solver(prb, scanax, scanay,
                             theta, det, voxelsize, energy, tomoshape)
+    def signal_handler(sig, frame):
+        print('Remove class and free gpu memory')
+        slv=[]
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTSTP, signal_handler)
+    
 
-    # Project
-    psis = slv.fwd_tomo(obj.complexform)
-    psis = slv.exptomo(psis)
-    # Propagate
-    data = slv.fwd_ptycho(psis)
-    data = np.abs(data)**2
-    # Add noise
-    #data = np.random.poisson(data).astype('float32')
+    # Compute data  |FQ(exp(i\nu R x))|^2,
+    data = np.abs(slv.fwd_ptycho(slv.exptomo(slv.fwd_tomo(obj.complexform))))**2
+    print("sigma = ",np.amax(np.sqrt(data*det.x*det.y)))
 
-    h = np.ones(psis.shape, dtype='complex64', order='C')
-    psi = np.ones(psis.shape, dtype='complex64', order='C')
-    lamd = np.zeros(psi.shape, dtype='complex64', order='C')
-    mu = np.zeros([3, *obj.shape], dtype='complex64', order='C')
+    # Apply Poisson noise (warning: Poisson distribution is discrete, so the resulting values are integers)
+    # data = np.random.poisson(data).astype('float32')
+
+    # Initial guess
+    h = np.ones(tomoshape, dtype='complex64', order='C')
+    psi = np.ones(tomoshape, dtype='complex64', order='C')
+    lamd = np.zeros(tomoshape, dtype='complex64', order='C')
     x = objects.Object(np.zeros(obj.shape, dtype='float32', order='C'), np.zeros(
         obj.shape, dtype='float32', order='C'), voxelsize)
-   
+
     # ADMM
     x = slv.admm(data, h, psi, lamd, x, rho, gamma, eta, piter, titer, NITER)
 
@@ -101,4 +73,3 @@ if __name__ == "__main__":
     dxchange.write_tiff(x.beta,  'beta/beta')
     dxchange.write_tiff(x.delta,  'delta/delta')
 
-   
