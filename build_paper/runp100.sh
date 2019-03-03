72c23ffec8a7682e1c04cba3087faa616171ca4d
math-vrn <vnikitin90@gmail.com>
2019-03-03 10:38:06 -0600

    CG optimization

--- build_paper/runp100.sh
+++
@@ -1,4 +0,0 @@
-nohup python -u test_s.py 1 0 >res0 &
-CUDA_VISIBLE_DEVICES=4 nohup python -u test_s.py 0 1 >res1 &
-CUDA_VISIBLE_DEVICES=6 nohup python -u test_s.py 0 2 >res2 &
-CUDA_VISIBLE_DEVICES=5 nohup python -u test_s.py 0 3 >res3 &
