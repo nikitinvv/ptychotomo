2f5f9b0ce4cebf6f1d93c0800f24dc8c1c21c619
Viktor Nikitin <vnikitin@dictum.xray.aps.anl.gov>
2020-10-29 22:49:33 -0500

    add testnanomax

--- experimental/nanomax/simulate/testnanomax.py
+++ experimental/nanomax/simulate/testnanomax.py
@@ -5,7 +5,7 @@ import sys
 import cupy as cp
 import dxchange
 import numpy as np
-
+import scipy.ndimage as ndimage
 import ptychotomo as pt
 import ptychotomo.util as util
 
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     piter = 4  # ptychography iterations
     titer = 4  # tomography iterations
     diter = 4
-    niter = 256  # ADMM iterations
+    niter = 128  # ADMM iterations
     ptheta = 1
     pnz = 128  # number of slice partitions for simultaneous processing in tomography
     # Load a 3D object
@@ -55,9 +55,9 @@ if __name__ == "__main__":
                                                   2:64+nprb//2, 64-nprb//2:64+nprb//2]/det[0]/100
     theta = cp.load('model/theta.npy')[:ntheta]
     scan0 = cp.load('model/scan.npy')[:, :ntheta]
-    scan = cp.zeros([2,ntheta,1024],dtype='float32')
+    scan = cp.zeros([2,ntheta,2048],dtype='float32')
     for k in range(ntheta):       
-        scan[:,k,:] = scan0[:,k,sample(range(13689),1024)]    
+        scan[:,k,:] = scan0[:,k,sample(range(13689),2048)]    
 
     
     scan = scan*(n-nprb)/(scan.max())
@@ -68,13 +68,17 @@ if __name__ == "__main__":
     # Compute data
     psi = slv.fwd_tomo_batch(obj)
     exppsi = slv.exptomo(psi)
-    mmin,mmax = util.find_min_max(exppsi.get())
     name = str(recover_prb)+str(swap_prb)+str(align)+str(shake)+str(nmodes)
     
+
     if(shake):
+        # s = np.zeros([ntheta,2],dtype='float32')
+        # for k in range(ntheta):
+        #     s[k] = np.int32((np.random.random(2)-0.5)*16)
+        # np.save('s',s)
+        s = np.load('s.npy')
         for k in range(ntheta):
-            s = np.int32((np.random.random(2)-0.5)*8)
-            exppsi[k] = cp.roll(exppsi[k],(s[0],s[1]),axis=(0,1))                    
+            exppsi[k] = cp.roll(exppsi[k],(s[k,0],s[k,1]),axis=(0,1))                    
 
     dxchange.write_tiff_stack(cp.angle(exppsi).get(),
                     'psiinit/psiangle'+name, overwrite=True)
@@ -91,22 +95,42 @@ if __name__ == "__main__":
     slv = pt.Solver(scan, theta, det, voxelsize,
                     energy, len(theta), nz, n, nprb, ptheta, pnz, nmodes)
     # Initial guess
-    # h1 = exppsi.copy()#cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1
-    h1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
-    
+    #h1 = exppsi.copy()#cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1
+    h1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j    
     h2 = cp.zeros([3, nz, n, n], dtype='complex64', order='C')
     h3 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
 
-    # psi1 = exppsi.copy()#cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1
-    psi1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
+
+    psi1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1
+    # psi1 = exppsi.copy()#cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
     psi2 = cp.zeros([3, nz, n, n], dtype='complex64', order='C')
+    # psi3 = exppsi.copy()#cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
     psi3 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1+1e-10j
     lamd1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')
     lamd2 = cp.zeros([3, nz, n, n], dtype='complex64', order='C')
     lamd3 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')    
+
     u = cp.zeros([nz, n, n], dtype='complex64', order='C')
     flow = np.zeros([ntheta, nz, n, 2], dtype='float32', order='C')
 
+
+    psi1, prb = slv.cg_ptycho_batch(
+                data, psi1, prb, scan, h1, lamd1, None, 128, model, recover_prb)
+    print(data.shape)
+    for k in range(ntheta):
+        
+        a = cp.angle(psi1[k])                    
+        a[a<0]=0
+        cm = ndimage.center_of_mass(a.get())                             
+        scan[0,k]-=(cm[1]-n//2)
+        scan[1,k]-=(cm[0]-nz//2)
+        ids = cp.where((scan[0,k]>n-1-nprb)+(scan[1,k]>nz-1-nprb))[0]
+        scan[0,k,ids]=-1
+        scan[1,k,ids]=-1
+        data[k,ids.get()] = 0
+               
+    psi1 = cp.zeros([ntheta, nz, n], dtype='complex64', order='C')+1                    
+
     # ADMM
     u, psi3, psi2, psi1, flow, prb = slv.admm(
-        data, psi3, psi2, psi1, flow, prb, scan, h3, h2, h1, lamd3, lamd2, lamd1, u, alpha, mmin, mmax, piter, titer, diter, niter, model, recover_prb, align, name)
+        data, psi3, psi2, psi1, flow, prb, scan, h3, h2, h1, lamd3, lamd2, lamd1, u, alpha, piter, titer, diter, niter, model, recover_prb, align, name)
