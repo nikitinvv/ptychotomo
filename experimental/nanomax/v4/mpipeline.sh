a33ecb27987ac7c20e5ef01a8740402d9f2a829d
math-vrn <vnikitin90@gmail.com>
2021-04-29 10:42:57 -0500

    update

--- experimental/nanomax/v4/mpipeline.sh
+++ experimental/nanomax/v4/mpipeline.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # source /home/vvnikitin/anaconda3/etc/profile.d/conda.sh
 # conda activate tomoalign
-
-python read_data.py /gdata/RAVEN/vnikitin/nanomax/
-python sort.py /gdata/RAVEN/vnikitin/nanomax/
+prefix=/gdata/RAVEN/vnikitin/nanomax/ 
+python read_data.py $prefix
+python sort.py $prefix
 for k in {0..174..8}; 
 do
     for j in {0..7}; 
     do
         echo $(($k+$j))
-        CUDA_VISIBLE_DEVICES=$j python rec_full.py /gdata/RAVEN/vnikitin/nanomax/ $(($k+$j)) 2000 1 16 &                
+        CUDA_VISIBLE_DEVICES=$j python rec_full.py $prefix $(($k+$j)) 2000 1 16 &                
     done
     wait
 done
-python prealign_sift1.py /gdata/RAVEN/vnikitin/nanomax/ 2000
+python prealign_sift1.py $prefix 2000
 
 
 for k in {0..174..8}; 
@@ -21,18 +21,18 @@ do
     for j in {0..7}; 
     do
         echo $(($k+$j))
-        CUDA_VISIBLE_DEVICES=$j python rec_crop.py /gdata/RAVEN/vnikitin/nanomax/ $(($k+$j)) 2700 1 64 &                
+        CUDA_VISIBLE_DEVICES=$j python rec_crop.py $prefix $(($k+$j)) 2700 1 64 &                
     done
     wait
 done
-python prealign_sift2.py /gdata/RAVEN/vnikitin/nanomax/ 2700
+python prealign_sift2.py $prefix 2700
 
 for k in {0..174..8}; 
 do
     for j in {0..7}; 
     do
         echo $(($k+$j))
-        CUDA_VISIBLE_DEVICES=$j python rec_crop_final.py /gdata/RAVEN/vnikitin/nanomax/  $(($k+$j)) 3500 1 32 &                
+        CUDA_VISIBLE_DEVICES=$j python rec_crop_final.py $prefix  $(($k+$j)) 3500 1 32 &                
     done
     wait
 done
